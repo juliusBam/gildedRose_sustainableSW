2f82e131024a967c45019d10f17f28bced4e32ad
juliob <juliobampi97@gmail.com>
2024-01-24 11:50:03 +0100

    Written test for default behaviour of a normal item

---
+++ csharpcore/GildedRoseTests/DefaultItemTest.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using GildedRoseKata;
+using NUnit.Framework;
+
+namespace GildedRoseTests;
+
+public class DefaultItemTest
+{
+
+    private List<Item> _getDefaultItems()
+    {
+        return new List<Item> { new Item {Name = "Vanilla", SellIn = 10, Quality = 30} };
+    }
+
+    
+    //Test sinking of quality and sellIn under default conditions
+    [Test]
+    public void TestDefaultBehaviour()
+    {
+        //given
+        var items = this._getDefaultItems();
+        GildedRose gildedRose = new GildedRose(items);
+        
+        //when 10 days passed
+        for (int days = 0; days < 10; days++)
+        {
+            gildedRose.UpdateQuality();
+        }
+        
+        //then
+        Assert.AreEqual(items[0].SellIn, 0);
+        Assert.AreEqual(items[0].Quality, 20);
+
+    }
+    
+}
