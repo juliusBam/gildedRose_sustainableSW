45b380596de5c11ad7eb90b657693facb1884640
juliob <juliobampi97@gmail.com>
2024-01-24 12:33:36 +0100

    Added test for BackstagePasses for limit of double quality increase

--- csharpcore/GildedRoseTests/BackStagePassesTest.cs
+++ csharpcore/GildedRoseTests/BackStagePassesTest.cs
@@ -74,4 +74,25 @@ public class BackStagePassesTest
 
     }
     
+    //the increase of quality of backstage passes should be normal
+    //until the 10th day before the concert, afterwards it will be double until the fifth day before the concert
+    [Test]
+    public void TestLimitDoubleIncreaseOfQuality()
+    {
+        //given
+        var items = this._getBackstagePasses();
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when 11 days passed
+        for (int days = 0; days < 15; days++)
+        {
+            gildedRose.UpdateQuality();
+        }
+        
+        //then quality should have increased normally for 10 days and double for 1 day
+        Assert.AreEqual(40, items[0].Quality);
+        Assert.AreEqual(5, items[0].SellIn);
+
+    }
+    
 }
